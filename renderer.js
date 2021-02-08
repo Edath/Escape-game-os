f44f33f3d5f6ef4da0bc3b5297cfed7efad0e2e6
Elise <elisehallaert@gmail.com>
2021-02-08 13:32:23 +0100

    application base

--- renderer.js
+++ renderer.js
@@ -1,27 +1,30 @@
-// const myNotification = new Notification('Title', {
-//     body: 'Notification from the Renderer process'
-//   })
-  
-//   myNotification.onclick = () => {
-//     console.log('Notification cliquée')
-//   }
-  
-// const {electron} = require('electron');
+
 const {remote} = require('electron');
 const { desktopCapturer } = require('electron/common');
 const ipc = require('electron').ipcRenderer;
 
 const password_form = document.getElementById('first-password');
+const trash = document.getElementById('trash');
+const folder = document.getElementById('folder');
+const navigator = document.getElementById('folder');
 
 
 password_form.addEventListener('submit', function(){
-
   ipc.send('enter-desktop', document.getElementById('password').value)
 })
 
 ipc.on('ok', function(event, arg){
-  console.log('b')
   var win = remote.getCurrentWindow();
   win.close();
+})
 
+trash.addEventListener('click', function(){
+
+})
+
+folder.addEventListener('click', function(){
+
+})
+navigator.addEventListener('click', function(){
+  
 })
