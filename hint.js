dd04966ebe85681aa25a0a488e9ba3f82bbe6ca4
Elise <elisehallaert@gmail.com>
2021-02-08 13:32:09 +0100

    json managing

--- hint.js
+++ hint.js
@@ -1,5 +1,11 @@
 
 function get_value(name){
-    const data = require('./hint.json');
-    return data[name];
+    const fs = require('fs');
+}
+
+// No
+function set_value(value, name){
+    const data=require('./hint.json');
+    data[name]=value;
+
 }
