99a263e97c5b2a3e5edf439cdd3861c3ae7861e1
Elise <elisehallaert@gmail.com>
2021-02-16 11:47:03 +0100

    popover finished!

--- renderer_mail.js
+++ renderer_mail.js
@@ -6,11 +6,15 @@ const mail = document.getElementById('mail');
 const object = document.getElementById('object');
 const money = document.getElementById('money');
 
+const popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
+const popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl);
+});
+
 mail.addEventListener('click', function () {
   const mail = document.getElementById('mail');
   if (!mail.classList.contains('validated')) {
     mail.classList.add('validated');
-  } else {
     const object = document.getElementById('object');
     const money = document.getElementById('money');
     if (object.classList.contains('validated') && money.classList.contains('validated')) {
@@ -26,7 +30,6 @@ object.addEventListener('click', function () {
   const object = document.getElementById('object');
   if (!object.classList.contains('validated')) {
     object.classList.add('validated');
-  } else {
     const mail = document.getElementById('mail');
     const money = document.getElementById('money');
     if (mail.classList.contains('validated') && money.classList.contains('validated')) {
@@ -42,7 +45,6 @@ money.addEventListener('click', function () {
   const money = document.getElementById('money');
   if (!money.classList.contains('validated')) {
     money.classList.add('validated');
-  } else {
     const mail = document.getElementById('mail');
     const object = document.getElementById('object');
     if (mail.classList.contains('validated') && object.classList.contains('validated')) {
